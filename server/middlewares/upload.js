f8785c618852ab50a16ace3aef10d36e3d131183
s4hil <iffcogc34@gmail.com>
2025-09-24 12:36:30 +0530

    Update upload.js

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -36,7 +36,7 @@ const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB file size limit
+    fileSize: 10 * 1024 * 1024 // 5MB file size limit
   }
 });
 
